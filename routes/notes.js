66b7639cdded2d3239cc126eaf3eeeffdfc74b50
Savannah Fausto <savannahfausto@Savannahs-MBP.attlocal.net>
2022-09-24 08:31:25 -0700

    modularize server.js file with routes file and adds helper function to read, write and append with fs, bugs still need to be fixed

---
+++ routes/notes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+
+
+router.get('/', (_req, res) => {
+    readFromFile('./db/db.json').then((data) =>
+    res.json(JSON.parse(data))
+  );
+});
+
+router.post('/', (req, res) => {
+    console.info(`${req.method} request received to add a review`);
+
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        }
+        
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully 🚀`);
+    } else {
+        res.error('Error in adding note');
+    }
+
+})
+
+router.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const api = require('./routes/index.js');
+
 
 const PORT = 3001;
 
@@ -11,6 +10,7 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -22,43 +22,7 @@ app.get('/notes', (_req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/api/notes', (_req, res) => {
-    fs.readFile('./db/db.json', function(err, data){
-        if (err) throw err;
-        const parsedData = JSON.parse(data);
-        res.json(parsedData);
-    })
-});
 
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a review`);
-
-    const { title, text } = req.body;
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-            id: uuidv4()
-        }
-        
-        fs.readFile('./db/db.json', function(err, data){
-            if (err) throw err;
-            const parsedData = JSON.parse(data);
-            parsedData.push(newNote);
-            const noteString = JSON.stringify(parsedData)
-
-            fs.writeFile(`./db/db.json`, noteString, (err) => {
-                if (err) throw err;
-                res.json(parsedData)
-            });
-        })
-
-
-    }
-
-})
-
-app.delete()
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
   });
